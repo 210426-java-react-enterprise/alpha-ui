997b7e56f1452227c37f6d8f4a4f365a8c54e0d2
diuguide <everett.diuguid@gmail.com>
2021-06-07 12:26:13 -0400

    pushed up demo

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,66 @@
 import './App.css';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [loginCreds, setLoginCreds] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginCreds({ ...loginCreds, [name]: value });
+    console.log(loginCreds);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(loginCreds);
+    axios.post("someEndpoint", loginCreds).then(res => )
+  }
+
+
 
   return (
     <Container>
       <Row>
         <Col>
-        
-       
+          <Form>
+            <Form.Group>
+              <Form.Control
+                type="email"
+                id="username"
+                name="username"
+                value={loginCreds.username}
+                onChange={handleChange}
+                placeholder="Enter email"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                type="password"
+                id="password"
+                name="password"
+                value={loginCreds.password}
+                onChange={handleChange}
+                placeholder="Password"
+              />
+            </Form.Group>
+            <Button
+              onClick={handleSubmit}
+              variant="primary"
+              type="submit"
+              block
+            >
+             Login
+            </Button>
+          </Form>
         </Col>
       </Row>
-  </Container>  
-
+    </Container>
   );
 }
 
