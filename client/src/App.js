5f31e8b42f63699ac062a9b16e9e36252b50e037
diuguide <everett.diuguid@gmail.com>
2021-06-10 08:56:41 -0400

    minor edit for pull request

--- client/src/App.js
+++ client/src/App.js
@@ -5,29 +5,26 @@ import SignIn from "./components/SignIn";
 import SearchBar from "./components/SearchBar";
 import SignOut from "./components/SignOut";
 
-
 function App() {
-  
   return (
     <Container fluid>
       <Row className="d-flex justify-content-center">
         <Col lg={10}>
-        <Row>
-        <Col>
-          <Register />
-        </Col>
-        <Col>
-          <SignIn />
+          <Row>
+            <Col>
+              <Register />
+            </Col>
+            <Col>
+              <SignIn />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <SearchBar />
+            </Col>
+          </Row>
         </Col>
       </Row>
-      <Row>
-        <Col>
-          <SearchBar />
-        </Col>
-      </Row>
-        </Col>
-      </Row>
-      
     </Container>
   );
 }
